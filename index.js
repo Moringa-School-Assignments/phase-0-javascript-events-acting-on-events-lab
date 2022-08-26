c5c9ae532c0568b4edaad1be5ab53117b24cd700
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-08-26 23:45:56 +0300

    complete test

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    else if (event.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+  });
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left < 360){
+        dodger.style.left = `${left + 1}px`;
+    }
+
+
+//  return
+}
+function moveDodgerLeft(){
+    // return
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+    dodger.style.left = `${left - 1}px`;
+
+   }
